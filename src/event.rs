b5f17bf46d51ba6f3176b2071110c3a9aa614da2
oberien <jaro.fietz@gmx.de>
2016-06-25 19:14:00 +0200

    fix(event): Remove unused variable and debug prints

--- src/event.rs
+++ src/event.rs
@@ -42,12 +42,10 @@ impl HandleKey for FlashHandler {
 
     fn handle(&mut self, evt: &KeyEvent, keyboard: &mut Keyboard) -> UsbResult<()> {
         match evt {
-            &KeyEvent::KeyPressed(ref k) => {
-                println!("Key pressed: {:?}", k);
+            &KeyEvent::KeyPressed(_) => {
                 keyboard.set_all_colors(Color::new(255, 0, 0))
             },
-            &KeyEvent::KeyReleased(ref k) => {
-                println!("Key released: {:?}", k);
+            &KeyEvent::KeyReleased(_) => {
                 keyboard.set_all_colors(Color::new(0, 0, 255))
             },
         }
